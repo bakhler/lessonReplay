288b27efb4eca838cb1f7a05322a977b6074f233
bakhler <thehenridit@gmail.com>
2022-11-07 07:27:08 +0200

    Use types and create let veriables

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ console.log(false)
 console.log(null)
 console.log(undefined)
 console.log(300-200)
+
+let userFirstName ='Tom';
+userFirstName = 'Bred';
+userFirstName = "O'Donel";
+userFirstName = 'O\'Done1';
+
+console.log(userFirstName)
+
+let isSunInSkyNow = false 
